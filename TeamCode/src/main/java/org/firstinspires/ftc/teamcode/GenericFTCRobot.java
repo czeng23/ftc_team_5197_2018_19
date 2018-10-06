33ec6a89456a41e434a617e9d94baff28bd4055b
Lorenzo Pedroza <lorenzo.apedroza2020@gmail.com>
2018-10-06 15:53:44 -0700

    Added GenericFTCRobot class. I still need to modularize more hardware components.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GenericFTCRobot.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public abstract class GenericFTCRobot extends OpMode {
+    HardwareMap hwMap = null;
+
+}
