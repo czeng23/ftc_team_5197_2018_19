e9543f122dbdeed4acc0abd822cc599719312e36
Lorenzo Pedroza <lorenzo.apedroza2020@gmail.com>
2018-11-24 22:54:09 -0800

    Modularized a class with Rover Ruckus Vuforia.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaRoverRuckusLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaRoverRuckusLocalizer.java
@@ -4,12 +4,11 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 import java.util.ArrayList;
@@ -17,6 +16,7 @@ import java.util.List;
 
 import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
 import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.YZX;
 import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
 
 /**
@@ -25,7 +25,7 @@ import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.
  * @author Lorenzo Pedroza
  * Version history
  * ======= ======
- * v 0.1  11/24/18 @author Lorenzo Pedroza. Created file. Has license key.
+ * v 0.1  11/24/18 @author Lorenzo Pedroza. Created file. Has license key. //TODO test this.
  */
 
 
@@ -55,11 +55,19 @@ public class VuforiaRoverRuckusLocalizer implements FTCModularizableSystems{
     private OpenGLMatrix blueRoverLocationOnField;
     private OpenGLMatrix redFootprintLocationOnField;
     private OpenGLMatrix frontCratersLocationOnField;
-    private OpenGLMatrix baclSpaceLocationOnField;
+    private OpenGLMatrix backSpaceLocationOnField;
+
+    private OpenGLMatrix phoneLocationOnRobot;
 
     private final int CAMERA_FORWARD_DISPLACEMENT;
     private final int CAMERA_VERTICAL_DISPLACEMENT;
     private final int CAMERA_LEFT_DISPLACEMENT;
+
+    OpenGLMatrix robotLocationTransform = null;
+
+    VectorF translation = null;
+
+    Orientation rotation = null;
 
 
     VuforiaRoverRuckusLocalizer(final VuforiaLocalizer.CameraDirection CAMERA_CHOICE, final int CAMERA_FORWARD_DISPLCACEMENTInmm,
@@ -87,32 +95,105 @@ public class VuforiaRoverRuckusLocalizer implements FTCModularizableSystems{
                         DEGREES, 90, 0, 0));
         blueRover.setLocation(blueRoverLocationOnField);
 
-        OpenGLMatrix redFootprintLocationOnField = OpenGLMatrix
+        redFootprintLocationOnField = OpenGLMatrix
                 .translation(0, -mmFTCFieldWidth, mmTargetHeight)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 180));
         redFootprint.setLocation(redFootprintLocationOnField);
 
-        OpenGLMatrix frontCratersLocationOnField = OpenGLMatrix
+        frontCratersLocationOnField = OpenGLMatrix
                 .translation(-mmFTCFieldWidth, 0, mmTargetHeight)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0 , 90));
         frontCraters.setLocation(frontCratersLocationOnField);
 
-        OpenGLMatrix backSpaceLocationOnField = OpenGLMatrix
+        backSpaceLocationOnField = OpenGLMatrix
                 .translation(mmFTCFieldWidth, 0, mmTargetHeight)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, -90));
         backSpace.setLocation(backSpaceLocationOnField);
 
+        phoneLocationOnRobot = OpenGLMatrix
+                .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, YZX, DEGREES,
+                        parameters.cameraDirection == VuforiaLocalizer.CameraDirection.FRONT ? 90 : -90,
+                        0, 0));
 
+        for (VuforiaTrackable trackable : allTrackables){
+            ((VuforiaTrackableDefaultListener)trackable.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
+        }
 
-
-
-
-
-
-
+        targetsRoverRuckus.activate();
 
     }
 
+    public String currentVisibleRoverRuckusVuforiaTrackable(){
+        String currentVisibleTraclableName = "none visible";
+        for (VuforiaTrackable trackable : allTrackables) {
+            if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
+                targetVisible = true;
 
+                // getUpdatedRobotLocation() will return null if no new information is available since
+                // the last time that call was made, or if the trackable is not currently visible.
+                robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
+                if (robotLocationTransform != null) {
+                    lastLocation = robotLocationTransform;
+                }
+                currentVisibleTraclableName = trackable.getName();
+                break;
+            }
+        }
+        return currentVisibleTraclableName;
+    }
 
+    public float getRobotX(){
+        currentVisibleRoverRuckusVuforiaTrackable(); //no need to collect output. Just to run it to get visible vumark
+        if (targetVisible){
+            translation = lastLocation.getTranslation();
+            return translation.get(0)/mmPerInch;
+        }
+        return -1;
+    }
+
+    public float getRobotY(){
+        currentVisibleRoverRuckusVuforiaTrackable();
+        if (targetVisible){
+            translation = lastLocation.getTranslation();
+            return translation.get(1)/mmPerInch;
+        }
+        return -1;
+    }
+
+    public float getRobotZ(){
+        currentVisibleRoverRuckusVuforiaTrackable();
+        if (targetVisible){
+            translation = lastLocation.getTranslation();
+            return translation.get(2)/mmPerInch;
+        }
+        return -1;
+    }
+
+    public float getRobotRoll(){
+        currentVisibleRoverRuckusVuforiaTrackable();
+        if (targetVisible){
+            rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
+            return rotation.firstAngle;
+        }
+        return -1;
+    }
+
+    public float getRobotPitch(){
+        currentVisibleRoverRuckusVuforiaTrackable();
+        if (targetVisible){
+            rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
+            return rotation.secondAngle;
+        }
+        return -1;
+    }
+
+    public float getRobotHeading(){
+        currentVisibleRoverRuckusVuforiaTrackable();
+        if (targetVisible){
+            rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
+            return rotation.thirdAngle;
+        }
+        return -1;
+    }
 }
